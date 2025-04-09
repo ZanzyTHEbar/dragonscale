9be42858b005c87b433336d45c918e9f8b7d3227
zacariahheim <zacariahheim@gmail.com>
2025-04-09 22:12:18 +0000

    chore: minor nix changes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,7 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "unstable"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
@@ -11,14 +11,13 @@
   ];
 
   # Sets environment variables in the workspace
-  env = {};
+  env = { };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       "esbenp.prettier-vscode"
       "golang.go"
       "tamasfe.even-better-toml"
-      "jnoortheen.nix-ide"
       "zxh404.vscode-proto3"
       "shakram02.bash-beautify"
     ];
